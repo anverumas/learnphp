a0c4aa77c02d7a9cde4af94f11b229d193592f1c
anverumas <155238715+anverumas@users.noreply.github.com>
2026-09-04 09:58:42 +0300

    Update index.php

--- learnphp/index.php
+++ learnphp/index.php
@@ -1,4 +1,10 @@
 <?php
 $test = 'Hello';
-$test = true
+$test = $test . 'world' . '!';
+$test .= '!!!!';
+$name = 'Andrei';
+$age = 20;
+$test = "$name is $age years old!";
+$test = "hello\n\nworld!";
+var_dump($test);
 ?>
