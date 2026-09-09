27ef8dc5db9329ad7c6a5eeb82b8899e063b6e00
anverumas <uppercas@gmail.com>
2026-09-09 11:31:53 +0300

    adding_arrays

--- index.php
+++ index.php
@@ -12,5 +12,18 @@ $numbers[2] = 10;
 var_dump($numbers[2]);
 $test = [1, 'sasd', true, [1, 2, 3]];
 var_dump($test[3][1]);
-$test = ['name' => 'Andrei', 'age' => 20, 1, 2, 3, 'color' => 'green'];
-var_dump($test);
+$test = ['name' => 'Andrei',
+ 'age' => 20,
+    1,
+    2,
+    3,
+    'color' => 'green',
+    5,
+    100 => 6,
+    7,
+];
+var_dump($test[3]);
+$combined = [1, 2, 3] + [4, 5, 6, 7];
+var_dump($combined);
+$text = implode('-', $test);
+var_dump($text);
