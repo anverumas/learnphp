80c311048a289964d5ebc196d343e381fa70db9a
anverumas <uppercas@gmail.com>
2026-09-09 11:08:51 +0300

    array

--- index.php
+++ index.php
@@ -5,6 +5,12 @@ $test .= '!!!!';
 $name = 'Andrei';
 $age = 20;
 $test = "$name is $age years old!";
-$test = "hello\n\nworld!";
+$test = "hello world!";
 var_dump($test);
-?>
+$numbers = [1, 2, 3, 4, 5];
+$numbers[2] = 10;
+var_dump($numbers[2]);
+$test = [1, 'sasd', true, [1, 2, 3]];
+var_dump($test[3][1]);
+$test = ['name' => 'Andrei', 'age' => 20, 1, 2, 3, 'color' => 'green'];
+var_dump($test);
