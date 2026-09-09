59b5dfcfcc23c3ac5165d344c9aa8eccee290df9
anverumas <uppercas@gmail.com>
2026-09-09 10:45:34 +0300

    Fix folder

---
+++ index.php
@@ -0,0 +1,10 @@
+<?php
+$test = 'Hello';
+$test = $test . 'world' . '!';
+$test .= '!!!!';
+$name = 'Andrei';
+$age = 20;
+$test = "$name is $age years old!";
+$test = "hello\n\nworld!";
+var_dump($test);
+?>
