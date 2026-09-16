8e200e3dae5ee6fc82b7a59c5d6b3c85127a9ef7
anverumas <uppercas@gmail.com>
2026-09-16 11:47:07 +0300

    more class

--- index.php
+++ index.php
@@ -2,15 +2,35 @@
 
 class Box {
     public $length;
-    public $width;
-    public $height;
+    private $width;
+    protected $height;
 
     public function volume() {
         return $this->length * $this->width * $this->height;
     }
 }
+
+$num1 = 1;
+$num2 = $num1;
+$num1 = 2;
+var_dump($num1, $num2);
+
 $box1 = new Box();
-$box1->length = 5;
-$box1->width = 3;
-$box1->height = 2;
-var_dump($box1->volume());
+$box1->width = 1;
+$box2 = clone $box1;
+$box1->width = 2;
+var_dump($box1, $box2);
+
+class MetalBox extends Box {
+    public $weightperunit;
+    public function mass() {
+        return $this->weightperunit * $this->volume();
+    }
+    public function test2() {
+        $this->height = 10;
+        var_dump($this->height);
+    }
+
+}
+$metal1 = new MetalBox();
+var_dump($metal1);
