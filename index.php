6b196c31447d78cd94cc7af3ec7ab75aeaaa0861
anverumas <uppercas@gmail.com>
2026-09-16 10:48:26 +0300

    foreach

--- index.php
+++ index.php
@@ -9,24 +9,6 @@ $test = "hello world!";
 var_dump($test);
 $numbers = [1, 2, 3, 4, 5];
 $numbers[2] = 10;
-var_dump($numbers[2]);
-$test = [1, 'sasd', true, [1, 2, 3]];
-var_dump($test[3][1]);
-$test = ['name' => 'Andrei',
- 'age' => 20,
-    1,
-    2,
-    3,
-    'color' => 'green',
-    5,
-    100 => 6,
-    7,
-];
-var_dump($test[3]);
-$combined = [1, 2, 3] + [4, 5, 6, 7];
-var_dump($combined);
-$text = implode('-', $test);
-var_dump($text); 
 function helloName($name='Nameless', $age=0){
     var_dump("Hello, $name! you are $age years old");
 }
@@ -40,57 +22,13 @@ if($num > 5) {
 else {
     var_dump('smaller');
 }
-$day = (int) date('w');
-var_dump($day);
-
-if($day == 0) {
-    var_dump('sunday');
-} else if($day == 1) {
-    var_dump('monday');
-} else if($day == 2) {
-    var_dump('tuesday');
-} else if($day == 3) {
-    var_dump('wednesday');
-} else if($day == 4) {
-    var_dump('thursday');
-} else if($day == 5 || $day == 6) {
-    var_dump('partyday');
-} 
-switch($day) {
-    case 0:
-        var_dump('sunday');
-        break;
-    case 1:
-        var_dump('monday');
-        break;
-    case 2:
-        var_dump('tuesday');
-        break;
-    case 3:
-        var_dump('wednesday');
-        break;
-    case 4:
-        var_dump('thursday');
-        break;
-    case 5:
-    case 6:
-        var_dump('Partyday');
-        break;
-    default:
-        var_dump('whatday');
+$fruits = ['apple', 'banana', 'orange'];
+for($i = 0; $i < count($fruits); $i++) {
+    var_dump($fruits[$i]);
 }
-for($i = 0; $i < 10; $i++) {
-    var_dump($i);
+foreach($fruits as $fruit) {
+    var_dump($fruit);
 }
-for($i = 9; $i < 10; $i--) {
-    var_dump($i);
+foreach($fruits as $key => $fruit) {
+    var_dump("$key, $fruit");
 }
-for($i = 1; $i < 1_000_000; $i*= 2) {
-    var_dump($i);
-}
-$time = time();
-$counter = 0;
-while(time + 1 > time()){
-    counter++;
-}
-var_dump($counter);
