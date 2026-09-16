60aa8f0f1640a31fa952bc761e1bd62da8908f3e
anverumas <uppercas@gmail.com>
2026-09-16 11:07:18 +0300

    class

--- index.php
+++ index.php
@@ -1,34 +1,16 @@
 <?php
-$test = 'Hello';
-$test = $test . 'world' . '!';
-$test .= '!!!!';
-$name = 'Andrei';
-$age = 20;
-$test = "$name is $age years old!";
-$test = "hello world!";
-var_dump($test);
-$numbers = [1, 2, 3, 4, 5];
-$numbers[2] = 10;
-function helloName($name='Nameless', $age=0){
-    var_dump("Hello, $name! you are $age years old");
+
+class Box {
+    public $length;
+    public $width;
+    public $height;
+
+    public function volume() {
+        return $this->length * $this->width * $this->height;
+    }
 }
-helloName();
-$num = 10;
-if($num > 5) {
-    var_dump('bigger');
-} elseif ($num == 5) {
-    var_dump('equal');
-}
-else {
-    var_dump('smaller');
-}
-$fruits = ['apple', 'banana', 'orange'];
-for($i = 0; $i < count($fruits); $i++) {
-    var_dump($fruits[$i]);
-}
-foreach($fruits as $fruit) {
-    var_dump($fruit);
-}
-foreach($fruits as $key => $fruit) {
-    var_dump("$key, $fruit");
-}
+$box1 = new Box();
+$box1->length = 5;
+$box1->width = 3;
+$box1->height = 2;
+var_dump($box1->volume());
